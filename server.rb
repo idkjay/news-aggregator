01b4723825d6b0bef881bf8b0c6f234403061bab
Jay <jordanchu95@gmail.com>
2019-12-17 13:31:09 -0500

    sinatra app template

---
+++ server.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+require 'pry'
+require 'csv'
+
+
+set :bind,'0.0.0.0'
+
+get "/" do
+  erb :index
+end
---
+++ spec/test_spec.rb
@@ -0,0 +1,9 @@
+# test_spec.rb
+
+require "spec_helper"
+
+RSpec.describe "test" do
+  it "tests the test suite" do
+    expect(true).to eq true
+  end
+end
